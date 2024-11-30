67de3a8aac9e61cacdd24a5a36e409646ba74e96
AnikY-wq <aniketyelsalwar11@gmail.com>
2024-11-30 11:29:50 +0530

    fix: add route fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import { config } from "../utils/constants"
 
 export default function Home() {
     return (
         <>
             <h1>Welcome Player</h1>
             <button
-                onClick={() => { window.location.href = `http://localhost:5173/chess-game/play` }}
+                onClick={() => { window.location.href = `${config.home}/play` }}
             >Let's Play Chess</button >
         </>
     )
---
+++ src/utils/constants.jsx
@@ -0,0 +1,4 @@
+export const config = {
+    home: "https://aniky-wq.github.io/chess-game", // dev
+    // home: "http://localhost:5173/chess-game" // local
+}
